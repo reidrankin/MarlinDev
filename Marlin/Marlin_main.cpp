ac427a7242258ab84b500a6499ef97cf03cbec43
Reid Rankin <reidrankin@gmail.com>
2015-09-21 10:47:26 -0500

    Restyle G20/G21/M149 code to new astyle guidelines

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -255,11 +255,11 @@ static int commands_in_queue = 0;
 static char command_queue[BUFSIZE][MAX_CMD_SIZE];
 
 #if ENABLED(USE_G20G21)
-float linear_unit_modifier = 1.0;
-float volumetric_unit_modifier = 1.0;
+  float linear_unit_modifier = 1.0;
+  float volumetric_unit_modifier = 1.0;
 #endif
 #if ENABLED(USE_M149)
-TempUnit input_temp_units = TEMPUNIT_C;
+  TempUnit input_temp_units = TEMPUNIT_C;
 #endif
 
 const float homing_feedrate[] = HOMING_FEEDRATE;
@@ -941,13 +941,13 @@ inline bool code_value_bool() { return code_value_byte() > 0; }
 #if ENABLED(USE_G20G21)
 inline void set_input_linear_units(LinearUnit units) {
   switch (units) {
-    case LINEARUNIT_INCH:
-      linear_unit_modifier = 25.4;
-      break;
-    case LINEARUNIT_MM:
-    default:
-      linear_unit_modifier = 1.0;
-      break;
+  case LINEARUNIT_INCH:
+    linear_unit_modifier = 25.4;
+    break;
+  case LINEARUNIT_MM:
+  default:
+    linear_unit_modifier = 1.0;
+    break;
   }
   volumetric_unit_modifier = pow(linear_unit_modifier, 3.0);
 }
@@ -976,37 +976,37 @@ inline float code_value_axis_units(int axis) { return code_value_float(); }
 #endif
 
 #if ENABLED(USE_M149)
-  inline void set_input_temp_units(TempUnit units) {
-    input_temp_units = units;
-  }
-  
-  float code_value_temp_abs() {
-    switch (input_temp_units) {
-      case TEMPUNIT_C:
-        return code_value_float();
-      case TEMPUNIT_F:
-        return (code_value_float() - 32) / 1.8;
-      case TEMPUNIT_K:
-        return code_value_float() - 272.15;
-      default:
-        return code_value_float();
-    }
-  }
+inline void set_input_temp_units(TempUnit units) {
+  input_temp_units = units;
+}
 
-  float code_value_temp_diff() {
-    switch (input_temp_units) {
-      case TEMPUNIT_C:
-      case TEMPUNIT_K:
-        return code_value_float();
-      case TEMPUNIT_F:
-        return code_value_float() / 1.8;
-      default:
-        return code_value_float();
-    }
+float code_value_temp_abs() {
+  switch (input_temp_units) {
+  case TEMPUNIT_C:
+    return code_value_float();
+  case TEMPUNIT_F:
+    return (code_value_float() - 32) / 1.8;
+  case TEMPUNIT_K:
+    return code_value_float() - 272.15;
+  default:
+    return code_value_float();
   }
+}
+
+float code_value_temp_diff() {
+  switch (input_temp_units) {
+  case TEMPUNIT_C:
+  case TEMPUNIT_K:
+    return code_value_float();
+  case TEMPUNIT_F:
+    return code_value_float() / 1.8;
+  default:
+    return code_value_float();
+  }
+}
 #else
-  inline float code_value_temp_abs() { return code_value_float(); }
-  inline float code_value_temp_diff() { return code_value_float(); }
+inline float code_value_temp_abs() { return code_value_float(); }
+inline float code_value_temp_diff() { return code_value_float(); }
 #endif
 
 inline millis_t code_value_millis() {
@@ -3717,13 +3717,10 @@ inline void gcode_M145() {
  * M149: Set temperature units
  */
 inline void gcode_M149() {
-  if (code_seen('C')) {
-    set_input_temp_units(TEMPUNIT_C);
-  } else if (code_seen('K')) {
-    set_input_temp_units(TEMPUNIT_K);
-  } else if (code_seen('F')) {
+  if (code_seen('C'))
+    set_input_temp_units(TEMPUNIT_C); else if (code_seen('K'))
+    set_input_temp_units(TEMPUNIT_K); else if (code_seen('F'))
     set_input_temp_units(TEMPUNIT_F);
-  }
 }
 #endif
 
@@ -5225,7 +5222,6 @@ void process_next_command() {
     case 20: //G20: Inch Mode
       gcode_G20();
       break;
-    
     case 21: //G21: MM Mode
       gcode_G21();
       break;
@@ -5418,9 +5414,9 @@ void process_next_command() {
       break;
 #endif
 #if ENABLED(USE_M149)
-  case 149:
-    gcode_M149();
-    break;
+    case 149:
+      gcode_M149();
+      break;
 #endif
 #if ENABLED(BLINKM)
     case 150: // M150
